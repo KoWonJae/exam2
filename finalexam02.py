1c567a5f4cd1d01a75e954e610c4be7395f96fc8
kwj <koj135011@gmail.com>
2020-06-24 10:22:57 +0900

    second

---
+++ finalexam02.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[44]:
+
+
+class Point:
+    __x = 0
+    __y = 0
+            
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+    
+    def __add__(self, other):
+        num = other.x
+        num2 = other.y
+        self.x = self.x + num
+        self.y = self.y + num2
+        return self.x, self.y
+        
+    
+    def distance(self, another):
+        num = pow(self.x - another.x, 2, 2) + pow(self.y - another.y, 2, 2)
+        num2 = pow(num, 0.5)
+        return num2
+        
+
+p1 = Point(1,1)     
+p2 = Point(2,2)
+print('add answer')
+print(p1 + p2)
+
+print('distance')
+a = p1.distance(p2)
+print(a)
+
